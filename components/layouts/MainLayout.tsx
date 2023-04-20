2936fec62d930b5c43d09505411e0a3957f115b6
Alvaro <aleiva97@gmail.com>
2023-04-20 16:02:08 -0400

    pre-deplot

--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -1,10 +1,14 @@
 import Head from 'next/head';
 import { Navbar } from '../Navbar';
-
-
 import styles from './MainLayout.module.css';
 
-export const MainLayout = ({ children }) => {
+import { FC } from "react";
+
+type PropsWithChildren = {
+  children: React.ReactNode
+}
+
+export const MainLayout: FC<PropsWithChildren> = ({ children }) => {
     return (
         <div className={styles.container}>
           
